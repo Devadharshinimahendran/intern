2459edb20974cffcf9a2695dfc29d91ea3aebe0f
Devadharshinimahendran <147681276+Devadharshinimahendran@users.noreply.github.com>
2024-08-12 11:50:06 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,73 @@
-const file=require('fs')  
-let name="deva"
-console.log(name);
-let filedata=file.readFileSync('data.txt').toString();
-console.log(filedata);
-file.renameSync('data.txt','filedata.txt')
-let filedata2=file.readFileSync('filedata.txt').toString();
-console.log(filedata2);
-file.writeFileSync('filedata.txt','this is file data wrote by Deva')
-// let filedata3=file.readFileSync('filedata.txt').toString();
-// console.log(filedata3);
-// file.appendFileSync('filedata.txt','hello world2')
-// let filedata4=file.readFileSync('filedata.txt').toString();
-// console.log(filedata4);
-// file.unlinkSync('filedata.txt')
-// file.rmdirSync('delet')n
+const express=require('express');
+const mongoose=require('mongoose')
+
+
+// step-1 connecting with mongoose
+async function connect()
+{
+  await mongoose.connect("mongodb+srv://dd1472945:deva1234@deva.55hpp.mongodb.net/");
+  console.log("db connected");
+}
+
+
+connect()
+
+
+const server=express();
+// step-2 creating schema
+const Phonebook= new mongoose.Schema(
+   {
+       name:String,
+       phone:String,
+       location:String
+   },
+   {
+     collection:'PHONEBOOK'
+   }
+)
+
+
+// step-3 creating model
+const Model= mongoose.model('PHONEBOOK',Phonebook)
+
+server.get('/',(req,res)=>{
+   res.send("app is working fine")
+})
+
+server.get('/db',async (req,res)=>{
+       const data = await Model.find({});
+       res.json(data)
+})
+
+server.get('/db/:id', async (req,res)=>{
+       const data= await Model.findOne({ _id: req.params.id })
+       console.log(data);
+       res.json(data)
+})
+
+// server.post(‘/postdata’)
+server.get('/postdata', async (req,res)=>{
+   const data=new Model()   // step1 creating instance of model
+   // step-2 declaring data using model instance (data)
+   data.name="deva"
+   data.phone="9043368998"
+   data.location="erode"
+
+
+   // step-3 saving the data created
+   await data.save()
+   console.log("data saved");
+   res.json(data)
+
+
+})
+
+
+// server.delete(‘/delete/:id’)
+server.get('/delete/:id',async (req,res)=>{
+       await Model.deleteOne({ _id : req.params.id})
+})
+
+server.listen('8000',()=>{
+   console.log("server is running");
+})
